62f4c40b421a6f331d6c710c5f5d5cc1983199bd
AlexzanderToney <alexzanderToney@gmail.com>
2026-06-15 12:18:40 +0200

    Deck import size increase

--- Server/server.js
+++ Server/server.js
@@ -13,7 +13,7 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/library', libraryRoutes);
